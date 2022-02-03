d1a7f4783838750b60544dc41ad21a74ebb6ee6b
Philip <philipmjobe@gmail.com>
2022-02-03 13:47:47 -0500

    adds answer for array_max.js

--- array_max.js
+++ array_max.js
@@ -2,3 +2,17 @@
 
 // Input: [5, 17, -4, 20, 12]
 // Output: 20
+
+function max(arr) {
+  let max = arr[0];
+  let i = 1;
+  while (i < arr.length) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    i += 1;
+  }
+  return max;
+}
+
+console.log(max([5, 17, -4, 20, 12]));
