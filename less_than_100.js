0cfc42162eaae92d2060b42401bbd4b7fa30f5e2
Philip <philipmjobe@gmail.com>
2022-02-03 13:12:53 -0500

    adds answer for less_than_100.js

---
+++ less_than_100.js
@@ -0,0 +1,17 @@
+// Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+// Input: [99, 101, 88, 4, 2000, 50]
+// Output: [99, 88, 4, 50]
+
+function lessThan(array) {
+  let newArray = [];
+
+  array.forEach(function (element) {
+    if (element < 100) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+}
+
+console.log(lessThan([99, 101, 88, 4, 2000, 50]));
