36375f1b807d3a093f6fa205f4d51256f7919709
Philip <philipmjobe@gmail.com>
2022-02-03 14:30:30 -0500

    adds answer for reverse_array.js

--- reverse_array.js
+++ reverse_array.js
@@ -2,3 +2,15 @@
 
 // Input: [1, 2, 3, 4, 5]
 // Output: [5, 4, 3, 2, 1]
+
+function reverse(arr) {
+  let reverse = [];
+  let i = arr.length - 1;
+  while (i >= 0) {
+    reverse.push(arr[i]);
+    i -= 1;
+  }
+  return reverse;
+}
+
+console.log(reverse([1, 2, 3, 4, 5]));
