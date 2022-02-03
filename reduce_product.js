fc51b1116dc8819c7736c7f24dd7a672978c4bd6
Philip <philipmjobe@gmail.com>
2022-02-03 14:24:23 -0500

    adds answer for reduce_porduct.js

--- reduce_product.js
+++ reduce_product.js
@@ -4,3 +4,15 @@
 // Output: 24
 
 // Explanation: (1 x 2 x 3 x 4) = 24
+
+function reduceProduct(arr) {
+  let product = arr[0];
+  let i = 0;
+  while (i < arr.length) {
+    product *= arr[i];
+    i += 1;
+  }
+  return product;
+}
+
+console.log(reduceProduct([1, 2, 3, 4]));
