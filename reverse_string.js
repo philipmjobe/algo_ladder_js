a09f2471f38b886c6d05f9035076499731042bc0
Philip <philipmjobe@gmail.com>
2022-02-03 14:42:36 -0500

    adds answer for reverse_string.js

--- reverse_string.js
+++ reverse_string.js
@@ -2,3 +2,15 @@
 
 // Input: “abcde”
 // Output: “edcba”
+
+function reverse(str) {
+  let i = 0;
+  let result = "";
+  while (i < str.length) {
+    result = str[i] + result;
+    i += 1;
+  }
+  return result;
+}
+
+console.log(reverse("abcde"));
