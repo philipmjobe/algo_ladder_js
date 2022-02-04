89af6a9bec0c35892efde167d3963555eec6f7cd
Philip <philipmjobe@gmail.com>
2022-02-04 14:44:54 -0500

    adds answer for reverse_words.js

--- reverse_words.js
+++ reverse_words.js
@@ -2,3 +2,15 @@
 
 // Input: “popcorn is so cool isn’t it yeah i thought so”
 // Output: “so thought i yeah it isn’t cool so is popcorn”
+
+function reverseWords(str) {
+  let reversedWords = "";
+  str = str.split(" ");
+  let i = str.length - 1;
+  while (i >= 0) {
+    reversedWords += str[i] + " ";
+    i -= 1;
+  }
+  return reversedWords;
+}
+console.log(reverseWords("popcorn is so cool isn’t it yeah i thought so"));
