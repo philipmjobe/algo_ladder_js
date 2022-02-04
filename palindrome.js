b10a4aa9987184ab4dd12e27830514df451ea51b
Philip <philipmjobe@gmail.com>
2022-02-04 14:33:07 -0500

    adds answer for palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,16 @@
 
 // Input: “baloney”
 // Output: false
+
+function palindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, "");
+  var len = str.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(palindrome("A man, a plan, a canal. Panama"));
