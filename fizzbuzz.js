cce85fbe2ee9e07894d8be6a6773643879cc21b3
Philip <philipmjobe@gmail.com>
2022-04-08 14:41:34 -0400

    adds answer for fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,3 +3,18 @@
 // If the number is divisible by 3, print out "FIZZ".
 // If the number is divisible by 5, print out "BUZZ".
 // If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+function fizzbuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzbuzz(100);
