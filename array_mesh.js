43ec0c035fda835e4ac1398210fa84f240996ed7
Philip <philipmjobe@gmail.com>
2022-06-24 14:04:59 -0400

    adds answer for array_mesh.js

--- array_mesh.js
+++ array_mesh.js
@@ -2,3 +2,21 @@
 
 // Input: ["a", "b", "c", "d"]
 // Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+function mesh(arr) {
+  let pairs = [];
+  let i = 0;
+  while (i < arr.length) {
+    let j = 0;
+    while (j < arr.length) {
+      if (i != j) {
+        pairs.push(arr[i] + arr[j]);
+      }
+      j++;
+    }
+    i++;
+  }
+  return pairs;
+}
+
+console.log(mesh(["a", "b", "c", "d"]));
