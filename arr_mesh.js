b7452bc12b69b040189c5111cd50915a23141317
Philip <philipmjobe@gmail.com>
2022-06-24 13:58:30 -0400

    adds answer for arr_mesh.js

--- arr_mesh.js
+++ arr_mesh.js
@@ -2,3 +2,19 @@
 
 // Input: ["a", "b", "c"], ["d", "e", "f", "g"]
 // Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+function mesh(arr1, arr2) {
+  let meshed = [];
+  let i = 0;
+  while (i < arr1.length) {
+    let j = 0;
+    while (j < arr2.length) {
+      meshed.push(arr1[i] + arr2[j]);
+      j++;
+    }
+    i++;
+  }
+  return meshed;
+}
+
+console.log(mesh(["a", "b", "c"], ["d", "e", "f", "g"]));
