367effcb31df895505ab4e0c1a07b91fdfa3b6cf
Philip <philipmjobe@gmail.com>
2022-06-24 14:34:28 -0400

    adds answer for largest_product.js

--- largest_product.js
+++ largest_product.js
@@ -2,3 +2,23 @@
 
 // Input: [5, -2, 1, -9, -7, 2, 6]
 // Output: 63 (-9 * -7)
+
+function largest(numArr) {
+  let soFar = 0;
+  let i = 0;
+  while (i < numArr.length) {
+    let j = 0;
+    while (j < numArr.length) {
+      if (i !== j) {
+        if (numArr[i] * numArr[j] > soFar) {
+          soFar = numArr[i] * numArr[j];
+        }
+      }
+      j++;
+    }
+    i++;
+  }
+  return soFar;
+}
+
+console.log(largest([5, -2, 1, -9, -7, 2, 6]));
