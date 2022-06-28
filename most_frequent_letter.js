d9277c1255a8e6ed42e1990d7b4104ce6a087c25
Philip <philipmjobe@gmail.com>
2022-06-28 14:51:43 -0400

    adds answer for most_frequent_letter.js

--- most_frequent_letter.js
+++ most_frequent_letter.js
@@ -2,3 +2,28 @@
 
 // Input: “peter piper picked a peck of pickled peppers”
 // Output: “p”
+
+function mostCommon(str) {
+  let letterCount = {};
+  let noSpaces = str.gsub(/\s+/, "");
+  let i = 0;
+  let mostCommonLetter = "";
+  let mostCommonCount = 0;
+
+  while (i < noSpaces.length) {
+    if (letterCount[noSpaces[i]]) {
+      letterCount[noSpaces[i]]++;
+    } else {
+      letterCount[noSpaces[i]] === 1;
+    }
+
+    if (letterCount[noSpaces[i]] > mostCommonCount) {
+      mostCommonCount = letterCount[noSpaces[i]];
+      mostCommonLetter = noSpaces[i];
+    }
+    i++;
+  }
+  mostCommonLetter;
+}
+
+console.log(mostCommon(“peter piper picked a peck of pickled peppers”))
