4658c11bca2c640d15b98238e486d22bc1503bec
Philip <philipmjobe@gmail.com>
2022-07-18 14:51:45 -0400

    adds order_menu.js and question

--- count_votes.js
+++ count_votes.js
@@ -1,7 +1,25 @@
 // Given an array of strings, return a hash that provides of a count of how many times each string occurs.
 
-// Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
-
 // Output: {"Dewey" => 6, "Truman" => 5}
 
 // Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+// const input = ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"];
+
+function count(arr) {
+  let votesHash = {};
+  arr.forEach(function () {
+    let lowercaseVote = arr.toString().toLowerCase();
+
+    if (votesHash[lowercaseVote]) {
+      votesHash[lowercaseVote]++;
+    } else {
+      votesHash[lowercaseVote] === 1;
+    }
+  });
+  votesHash;
+}
+
+console.log(
+  count(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
+);
