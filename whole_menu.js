de88afe2671e1260f04aab31741f68713481a7d4
Philip <philipmjobe@gmail.com>
2022-08-03 08:31:29 -0400

    adds answer for whole_menu.js

--- whole_menu.js
+++ whole_menu.js
@@ -1,7 +1,14 @@
 // Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
 
-// Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+var menu = { "hot dog": 2, hamburger: 3, "steak sandwich": 5, fries: 1, "cole slaw": 1, soda: 2 };
 
 // Output: 14
 
 // Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+let sum = 0;
+
+for (var value of Object.values(menu)) {
+  sum += value;
+}
+console.log(sum);
